5bf0ad724b9ad99b92f435512551d18e284e5f86
Ferris Lucas <ferris.lucas@gmail.com>
2013-07-08 21:35:19 -0500

    spin turret and radar to look for new victims when there is not a lock

--- bots/sir_ticks_a_lot_bot.rb
+++ bots/sir_ticks_a_lot_bot.rb
@@ -9,17 +9,15 @@ class SirTicksALotBot < RTanque::Bot::Brain
 
     if (lock = self.get_radar_lock)
       destroy_lock(lock)
-      #@desired_heading = nil
-    #else
-      #self.seek_lock
+    else
+      command.turret_heading = sensors.heading + (RTanque::Heading::ONE_DEGREE * 33)
+      command.radar_heading = command.turret_heading
     end
   end
 
   def destroy_lock(reflection)
-    #command.heading = reflection.heading
     command.radar_heading = reflection.heading
     command.turret_heading = reflection.heading
-    #command.speed = reflection.distance > 200 ? MAX_BOT_SPEED : MAX_BOT_SPEED / 2.0
     if (reflection.heading.delta(sensors.turret_heading)).abs < TURRET_FIRE_RANGE
       command.fire(reflection.distance > 200 ? MAX_FIRE_POWER : MIN_FIRE_POWER)
     end
