d38653d4a394343aa833ed5f1a4e764b806451e9
Ferris Lucas <ferris.lucas@gmail.com>
2013-07-08 21:22:49 -0500

    make him shoot

--- bots/sir_ticks_a_lot_bot.rb
+++ bots/sir_ticks_a_lot_bot.rb
@@ -1,20 +1,38 @@
 class SirTicksALotBot < RTanque::Bot::Brain
   NAME = 'sir ticks a lot'
   include RTanque::Bot::BrainHelper
+  TURRET_FIRE_RANGE = RTanque::Heading::ONE_DEGREE * 5.0
 
   def tick!
-    ## main logic goes here
-    
-    # use self.sensors to detect things
-    # See http://rubydoc.info/github/awilliams/RTanque/master/RTanque/Bot/Sensors
-    
-    # use self.command to control tank
-    # See http://rubydoc.info/github/awilliams/RTanque/master/RTanque/Bot/Command
-    
-    # self.arena contains the dimensions of the arena
-    # See http://rubydoc.info/github/awilliams/RTanque/master/frames/RTanque/Arena
-
     command.heading = sensors.heading + (RTanque::Heading::ONE_DEGREE * 90)
-    command.speed = 10_000
+    command.speed = MAX_BOT_SPEED
+
+    if (lock = self.get_radar_lock)
+      destroy_lock(lock)
+      #@desired_heading = nil
+    #else
+      #self.seek_lock
+    end
+  end
+
+  def destroy_lock(reflection)
+    command.heading = reflection.heading
+    command.radar_heading = reflection.heading
+    command.turret_heading = reflection.heading
+    command.speed = reflection.distance > 200 ? MAX_BOT_SPEED : MAX_BOT_SPEED / 2.0
+    if (reflection.heading.delta(sensors.turret_heading)).abs < TURRET_FIRE_RANGE
+      command.fire(reflection.distance > 200 ? MAX_FIRE_POWER : MIN_FIRE_POWER)
+    end
+  end
+
+  def get_radar_lock
+    @locked_on ||= nil
+    lock = if @locked_on
+      sensors.radar.find { |reflection| reflection.name == @locked_on } || sensors.radar.first
+    else
+      sensors.radar.first
+    end
+    @locked_on = lock.name if lock
+    lock
   end
 end
